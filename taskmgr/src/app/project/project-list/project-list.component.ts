32e43fb87cf83db66f866789d57bce6e6928e129
xieshuhong <364118360@qq.com>
2017-09-08 13:22:35 +0800

    HostBinding(@routeAnim) state

--- taskmgr/src/app/project/project-list/project-list.component.ts
+++ taskmgr/src/app/project/project-list/project-list.component.ts
@@ -4,13 +4,14 @@ import { MdDialog } from '@angular/material';
 import { InviteComponent } from "../invite/invite.component";
 import { ConfirmDialogComponent } from "../../shared/confirm-dialog/confirm-dialog.component";
 import { slideToRight } from '../../anims/router.anim';
+import { listAnimation } from '../../anims/list.anim';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss'],
   animations: [
-    slideToRight
+    slideToRight, listAnimation
   ]
 })
 export class ProjectListComponent implements OnInit {
@@ -20,18 +21,21 @@ export class ProjectListComponent implements OnInit {
 
   projects = [
     {
+      "id": 1,
       "name":"企业协作平台",
       "desc":"这是一个企业内部项目",
       "coverImg":"assets/covers/0.jpg"
 
     },
     {
+      "id": 2,
       "name":"自动化测试项目",
       "desc":"这是一个企业内部项目",
       "coverImg":"assets/covers/1.jpg"
 
     },
     {
+      "id": 3,
       "name":"企业协作平台",
       "desc":"这是一个企业内部项目",
       "coverImg":"assets/covers/2.jpg"
@@ -45,7 +49,11 @@ export class ProjectListComponent implements OnInit {
 
   openNewProjectDialog() {
     const dialogRef = this.dialog.open(NewProjectComponent, {data: {title: '新增项目:'}});
-    dialogRef.afterClosed().subscribe( result => console.log(result));
+    dialogRef.afterClosed().subscribe( result => {
+      console.log(result);
+      this.projects = [... this.projects, {id: 4, name: '一个新项目', desc: '这是一个新项目', coverImg: 'assets/covers/8.jpg'}],
+      this.projects = [... this.projects, {id: 5, name: 'another新项目', desc: '这是a一个新项目', coverImg: 'assets/covers/9.jpg'}]      
+    });
   }
   launchInviteDialog(){
     const dialogRef = this.dialog.open(InviteComponent);
@@ -55,9 +63,12 @@ export class ProjectListComponent implements OnInit {
     
   }
 
-  launchConfirmDialog(){
+  launchConfirmDialog(project){
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {data: {title: '删除项目:', content: '您确认删除该项目吗?'}});
-    dialogRef.afterClosed().subscribe(result => console.log(result));
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+      this.projects = this.projects.filter(p => p.id !== project.id);
+    });
   }
 
 }
