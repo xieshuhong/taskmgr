e1d84caaca6ad13326a7bfd5447e1256464b6380
xieshuhong <364118360@qq.com>
2017-09-08 09:49:44 +0800

    keyframes与缓动函数

--- taskmgr/src/app/app.component.ts
+++ taskmgr/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { OverlayContainer } from "@angular/material";
-import { trigger, state, transition, style, animate } from '@angular/animations'
+import { trigger, state, transition, style, animate, keyframes } from '@angular/animations'
 
 @Component({
   selector: 'app-root',
@@ -9,10 +9,31 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
   animations: [
     trigger('square', 
       [
-        state('green', style({'background-color':'green', 'height': '100px', 'transform':'translateX(0)'})),
-        state('red',   style({'background-color':'red', 'height': '50px', 'transform':'translateX(100%)'})),
-        transition('green => red', animate(2000)),
-        transition('red => green', animate(2000))        
+        state('green', style({'background-color':'green', 'height': '100px', 'transform':'translateY(-100%)'})),
+        state('red',   style({'background-color':'red', 'height': '100px', 'transform':'translateY(100%)'})),
+        transition('green => red', animate('.3s cubic-bezier(0.19, 1, 0.22, 1)')),
+        transition('red => green', animate(5000,keyframes([
+          style({transform: 'translateY(100%)'}),
+          style({transform: 'translateY(98%)'}),
+          style({transform: 'translateY(95%)'}),
+          style({transform: 'translateY(90%)'}),
+          style({transform: 'translateY(80%)'}),
+          style({transform: 'translateY(60%)'}),
+          style({transform: 'translateY(30%)'}),
+          style({transform: 'translateY(0)'}),
+          style({transform: 'translateY(-10%)'}),
+          style({transform: 'translateY(-5%)'}),
+          style({transform: 'translateY(-2%)'}),
+          style({transform: 'translateY(0)'}),
+          style({transform: 'translateY(10%)'}),
+          style({transform: 'translateY(15%)'}),
+          style({transform: 'translateY(-15%)'}),          
+          style({transform: 'translateY(-40%)'}),          
+          style({transform: 'translateY(-80%)'}),          
+          style({transform: 'translateY(-90%)'}),
+          style({transform: 'translateY(-95%)'}),          
+          style({transform: 'translateY(100%)'})
+        ])))        
       ]
    )
   ]
--- taskmgr/src/app/app.module.ts
+++ taskmgr/src/app/app.module.ts
@@ -23,7 +23,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     TaskModule,
     BrowserAnimationsModule
   ],
-  providers: [TaskModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
